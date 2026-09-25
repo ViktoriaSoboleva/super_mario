c55a430291bb78713865d8305f6d931afa474294
Victoria Soboleva <vikhp1207@icloud.com>
2026-09-25 18:40:32 +0300

    Redesign second level

--- src/levels/second_level.cpp
+++ src/levels/second_level.cpp
@@ -20,10 +20,29 @@ biv::GameLevel* SecondLevel::get_next() {
 void SecondLevel::init_data() {
 	ui_factory->create_mario({39, 10}, 3, 3);
 	
-	ui_factory->create_ship({20, 25}, 40, 2);
-	ui_factory->create_ship({60, 20}, 10, 7);
-	ui_factory->create_ship({80, 25}, 20, 2);
-	ui_factory->create_ship({120, 20}, 10, 7);
-	ui_factory->create_ship({150, 25}, 40, 2);
-	ui_factory->create_ship({210, 20}, 10, 7);
+	ui_factory->create_ship({20, 25}, 35, 2);
+
+	ui_factory->create_ship({60, 20}, 12, 7);
+	ui_factory->create_ship({82, 25}, 18, 2);
+	
+	ui_factory->create_ship({150, 25}, 35, 2);
+
+	ui_factory->create_box({45, 17}, 8, 2);
+	ui_factory->create_full_box({86, 17}, 6, 3);
+
+	ui_factory->create_box({135, 13}, 10, 2);
+	ui_factory->create_full_box({160, 18}, 6, 3);
+
+	ui_factory->create_moving_platform({100, 18}, 12, 2);
+
+	ui_factory->create_ship({195, 20}, 12, 7);
+
+	ui_factory->create_enemy({30, 5}, 3, 2);
+	ui_factory->create_enemy({88, 5}, 3, 2);
+	ui_factory->create_enemy({155, 5}, 3, 2);
+
+	ui_factory->create_flying_enemy({75, 8}, 3, 2);
+
+	ui_factory->create_jumping_enemy({125, 10}, 3, 2);
+	ui_factory->create_jumping_enemy({170, 10}, 3, 2);
 }
